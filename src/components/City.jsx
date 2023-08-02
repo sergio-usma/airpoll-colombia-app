ba905c57b489090257affa8a3881d303b82208f6
Sergio Usma <sergioandresusma@hotmail.com>
2023-08-02 18:34:52 -0500

    Add City component basic structure

---
+++ src/components/City.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/city.module.css';
+
+export default function City() {
+  return (
+    <div className={styles.card}>
+      <h2>This is a selected city</h2>
+    </div>
+  );
+}
