8c23998905bd33cd1aa52e7b62a6af2f042247b2
Sergio Usma <sergioandresusma@hotmail.com>
2023-08-03 18:15:26 -0500

    Update Home component to render only the Feed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,8 @@
 import Feed from '../components/Feed';
-import SearchBar from '../components/SearchBar';
 
 export default function Home() {
   return (
     <>
-      <SearchBar />
       <Feed />
     </>
   );
