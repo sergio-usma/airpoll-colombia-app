ec20895a01dd0ee8f59ee2f096dbd6ea64f1b9e0
Sergio Usma <sergioandresusma@hotmail.com>
2023-08-02 19:39:23 -0500

    Add City component basic structure

---
+++ src/pages/City.jsx
@@ -0,0 +1,15 @@
+export default function City() {
+  return (
+    <div>
+      <h2> Air Quality Index:</h2>
+      <ul>
+        <li>Particulates PM2.5:</li>
+        <li>Carbon:</li>
+        <li>Nitrogen Dioxide:</li>
+        <li>Nitrogen Monoxide:</li>
+        <li>Sulpher Dioxide:</li>
+        <li>Ozone:</li>
+      </ul>
+    </div>
+  );
+}
