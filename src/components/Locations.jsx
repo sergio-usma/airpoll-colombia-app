4f89b774d03608602e705adbfb30f6267a6007c9
Sergio Usma <sergioandresusma@hotmail.com>
2023-08-03 10:34:29 -0500

    Edit Locations layout in the Feed

--- src/components/Locations.jsx
+++ src/components/Locations.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+import locationImg from '../assets/locations/santa_marta/santamarta_col_sm.jpg';
 import styles from '../styles/locations.module.css';
 
 export default function Locations() {
@@ -6,9 +7,27 @@ export default function Locations() {
   function handleClick() {
     navigate('/city');
   }
+
+  function handleKeyDown(event) {
+    if (event.keyCode === 13) {
+      handleClick();
+    }
+  }
+
   return (
-    <button className={styles.card} onClick={handleClick} type="button">
-      <h2 className={styles.card__title}>This is a selected location</h2>
-    </button>
+    <div
+      className={styles.card}
+      onClick={handleClick}
+      onKeyDown={handleKeyDown}
+      role="button"
+      tabIndex={0}
+    >
+      <img className={styles.card__img} src={locationImg} alt="This is a city" />
+      <div className={styles.card__details}>
+        <h2 className={styles.card__title}>city_name</h2>
+        <p className={styles.card__subtitle}>State</p>
+        <p className={styles.card__paragraph}>Population</p>
+      </div>
+    </div>
   );
 }
