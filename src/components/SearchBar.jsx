009d2aeb2cac74ae7a56c8faf04e52ed8551ba7f
Sergio Usma <sergioandresusma@hotmail.com>
2023-08-03 18:14:41 -0500

    Refactor SearchBar and fetch data for Locations comp

--- src/components/SearchBar.jsx
+++
@@ -1,9 +0,0 @@
-import style from '../styles/searchbar.module.css';
-
-export default function SearchBar() {
-  return (
-    <div className={style.hero}>
-      <input type="search" placeholder="Enter a city in Colombia" />
-    </div>
-  );
-}
