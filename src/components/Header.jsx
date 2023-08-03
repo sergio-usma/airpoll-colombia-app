b0de499dfafa03d37fc3d2157620c24c85e90e69
Sergio Usma <sergioandresusma@hotmail.com>
2023-08-02 19:09:51 -0500

    Add Links and NavLinks to the Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,23 @@
 import { BiArrowBack, BiMicrophone } from 'react-icons/bi';
 import { BsGearFill } from 'react-icons/bs';
+import { Link, NavLink } from 'react-router-dom';
 import styles from '../styles/header.module.css';
 
 export default function Header() {
   return (
     <header className={styles}>
       <div className={styles.container}>
-        <BiArrowBack fontSize="20px" color="#fff" />
+        <Link to="/">
+          <BiArrowBack fontSize="20px" color="#fff" />
+        </Link>
         <h1>The Air Pollution App</h1>
         <nav>
-          <BiMicrophone fontSize="20px" color="#fff" />
-          <BsGearFill fontSize="18px" color="#fff" />
+          <Link to="/">
+            <BiMicrophone fontSize="20px" color="#fff" />
+          </Link>
+          <NavLink to="/">
+            <BsGearFill fontSize="18px" color="#fff" />
+          </NavLink>
         </nav>
       </div>
     </header>
