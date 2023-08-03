ac0ee22307a2a4b8b60e359be2f5c74304d2d22e
Sergio Usma <sergioandresusma@hotmail.com>
2023-08-02 19:41:36 -0500

    Add Routes and paths to render City component as a dynamic page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
-import Feed from './components/Feed';
+import City from './pages/City';
 
 function App() {
   return (
     <>
       <Header />
-      <div>
-        <Home />
-        <Feed />
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/city" element={<City />} />
+      </Routes>
     </>
   );
 }
