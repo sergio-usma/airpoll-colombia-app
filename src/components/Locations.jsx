622d6c4d6fd38f1e250f1c0785285e6e2a5dec6c
Sergio Usma <sergioandresusma@hotmail.com>
2023-08-03 18:26:22 -0500

    Add propTypes and catch data from local JSON

--- src/components/Locations.jsx
+++ src/components/Locations.jsx
@@ -1,33 +1,30 @@
-import { useNavigate } from 'react-router-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from 'prop-types';
+import { BsFillPeopleFill } from 'react-icons/bs';
 import locationImg from '../assets/locations/santa_marta/santamarta_col_sm.jpg';
 import styles from '../styles/locations.module.css';
 
-export default function Locations() {
-  const navigate = useNavigate();
-  function handleClick() {
-    navigate('/city');
-  }
-
-  function handleKeyDown(event) {
-    if (event.keyCode === 13) {
-      handleClick();
-    }
-  }
-
+export default function Locations({
+  city, state, population, onClick,
+}) {
   return (
-    <div
-      className={styles.card}
-      onClick={handleClick}
-      onKeyDown={handleKeyDown}
-      role="button"
-      tabIndex={0}
-    >
+    <div className={styles.card} onClick={onClick} role="presentation">
       <img className={styles.card__img} src={locationImg} alt="This is a city" />
       <div className={styles.card__details}>
-        <h2 className={styles.card__title}>city_name</h2>
-        <p className={styles.card__subtitle}>State</p>
-        <p className={styles.card__paragraph}>Population</p>
+        <h2 className={styles.card__title}>{city}</h2>
+        <p className={styles.card__subtitle}>{state}</p>
+        <p className={styles.card__paragraph}>
+          {`${population.toLocaleString()} `}
+          <BsFillPeopleFill />
+        </p>
       </div>
     </div>
   );
 }
+
+Locations.propTypes = {
+  city: PropTypes.string.isRequired,
+  state: PropTypes.string.isRequired,
+  population: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
