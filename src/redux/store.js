7b3100d3dd76477ca84e874aa91ca6e63d81ee21
Sergio Usma <sergioandresusma@hotmail.com>
2023-08-03 15:36:27 -0500

    Connect detailsSlice reducer with the store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import detailsReducer from './getCitiesSlice';
 
 const store = configureStore({
   reducer: {
+    details: detailsReducer,
   },
 });
 
