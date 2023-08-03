9cba1373aa5eec764d3ca720f7b914e84845195f
Sergio Usma <sergioandresusma@hotmail.com>
2023-08-02 22:19:03 -0500

    Add Layout to the City component

--- src/pages/City.jsx
+++ src/pages/City.jsx
@@ -1,15 +1,57 @@
+import { FaMapLocationDot } from 'react-icons/fa6';
+import hero from '../assets/locations/santa_marta/santamarta_col_sm.jpg';
+import styles from '../styles/city.module.css';
+
 export default function City() {
   return (
-    <div>
-      <h2> Air Quality Index:</h2>
-      <ul>
-        <li>Particulates PM2.5:</li>
-        <li>Carbon:</li>
-        <li>Nitrogen Dioxide:</li>
-        <li>Nitrogen Monoxide:</li>
-        <li>Sulpher Dioxide:</li>
-        <li>Ozone:</li>
-      </ul>
+    <div className={styles.city__card}>
+      <div className={styles.city__header}>
+        <FaMapLocationDot fontSize="5.5rem" color="#fff" />
+        <div className={styles.city__info}>
+          <h2 className={styles.city__name}>Santa Marta</h2>
+        </div>
+      </div>
+      <div className={styles.city__image}>
+        <p className={styles.state__name}>Magdalena, Colombia</p>
+        <img className={styles} src={hero} alt="This is a banner" />
+        <h3 className={styles.city__aqi}>Air Quality Index:</h3>
+      </div>
+
+      <div className={styles.city__data}>
+        <h4 className={styles.table__header}>Air Quality Parameters</h4>
+        <table>
+          <tbody>
+            <tr>
+              <td>Particulates PM2.5:</td>
+              <td>2.5</td>
+            </tr>
+            <tr>
+              <td>Carbon:</td>
+              <td>2.5</td>
+            </tr>
+            <tr>
+              <td>Nitrogen Dioxide:</td>
+              <td>2.5</td>
+            </tr>
+          </tbody>
+
+          <tbody>
+            <tr>
+              <td>Nitrogen Monoxide:</td>
+              <td>2.5</td>
+            </tr>
+
+            <tr>
+              <td>Sulphur Dioxide:</td>
+              <td>2.5</td>
+            </tr>
+            <tr>
+              <td>Ozone:</td>
+              <td>2.5</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
