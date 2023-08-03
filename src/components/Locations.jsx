97d7b0ab8300fee26fe55f76f1f58c11c2f4b2c4
Sergio Usma <sergioandresusma@hotmail.com>
2023-08-02 23:14:02 -0500

    Add clickHandler function to render City path

--- src/components/Locations.jsx
+++ src/components/Locations.jsx
@@ -1,9 +1,14 @@
+import { useNavigate } from 'react-router-dom';
 import styles from '../styles/locations.module.css';
 
 export default function Locations() {
+  const navigate = useNavigate();
+  function handleClick() {
+    navigate('/city');
+  }
   return (
-    <div className={styles.card}>
+    <button className={styles.card} onClick={handleClick} type="button">
       <h2 className={styles.card__title}>This is a selected location</h2>
-    </div>
+    </button>
   );
 }
