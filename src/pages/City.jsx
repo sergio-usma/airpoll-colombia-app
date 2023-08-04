b1e00fa3b3129ccb2fbfbf955e97b64cd40b6516
Sergio Usma <sergioandresusma@hotmail.com>
2023-08-03 20:17:57 -0500

    Fetch current city information from the local JSON

--- src/pages/City.jsx
+++ src/pages/City.jsx
@@ -1,17 +1,41 @@
 import { FaMapLocationDot } from 'react-icons/fa6';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useRef } from 'react';
 import hero from '../assets/locations/santa_marta/santamarta_col_sm.jpg';
 import styles from '../styles/city.module.css';
+import { getCityDetail } from '../redux/getCitiesSlice';
 
 export default function City() {
+  const { city } = useParams();
+  const { cities } = useSelector((state) => state.details);
+  const dispatch = useDispatch();
+
+  const coordinates = useRef();
+
+  const currentCity = cities.filter((location) => location.capital === city);
+
+  currentCity.forEach((location) => {
+    coordinates.current = [location.latitude, location.longitude];
+  });
+
+  useEffect(() => {
+    dispatch(getCityDetail(coordinates.current));
+  }, [dispatch, city]);
+
   return (
     <div className={styles.city__card}>
       <div className={styles.city__title}>
         <FaMapLocationDot fontSize="4rem" color="#fff" />
-        <h2 className={styles.city__name}>Santa Marta</h2>
+        <h2 className={styles.city__name}>{currentCity[0].capital}</h2>
       </div>
       <div className={styles.city__image}>
-        <p className={styles.state__name}>Magdalena, Colombia</p>
-        <img className={styles} src={hero} alt="This is a banner" />
+        <p className={styles.state__name}>{`${currentCity[0].department}, Colombia`}</p>
+        <img
+          className={styles}
+          src={hero}
+          alt={`This is a landscape from ${currentCity[0].department}, Colombia`}
+        />
         <h3 className={styles.city__aqi}>Air Quality Index:</h3>
       </div>
 
@@ -20,32 +44,39 @@ export default function City() {
         <table>
           <tbody>
             <tr>
-              <td>Particulates PM2.5:</td>
-              <td>2.5 μg/m3</td>
+              <td>Ammonia:</td>
+              <td> μg/m3</td>
             </tr>
             <tr>
               <td>Carbon:</td>
-              <td>2.5 μg/m3</td>
+              <td> μg/m3</td>
             </tr>
             <tr>
               <td>Nitrogen Dioxide:</td>
-              <td>2.5 μg/m3</td>
+              <td> μg/m3</td>
+            </tr>
+            <tr>
+              <td>Nitrogen Monoxide:</td>
+              <td> μg/m3</td>
             </tr>
           </tbody>
 
           <tbody>
             <tr>
-              <td>Nitrogen Monoxide:</td>
-              <td>2.5 μg/m3</td>
+              <td>Ozone:</td>
+              <td> μg/m3</td>
             </tr>
-
+            <tr>
+              <td>Particulates PM2.5:</td>
+              <td> μg/m3</td>
+            </tr>
+            <tr>
+              <td>Particulates PM10:</td>
+              <td> μg/m3</td>
+            </tr>
             <tr>
               <td>Sulphur Dioxide:</td>
-              <td>2.5 μg/m3</td>
-            </tr>
-            <tr>
-              <td>Ozone:</td>
-              <td>2.5 μg/m3</td>
+              <td> μg/m3</td>
             </tr>
           </tbody>
         </table>
