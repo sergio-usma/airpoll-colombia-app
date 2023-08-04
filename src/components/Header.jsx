f037cbaa706bdd5ab33ac2ac76e93c1c4947cb47
Sergio Usma <sergioandresusma@hotmail.com>
2023-08-04 14:05:51 -0500

    Refactor Header component for testing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,18 @@
-import { BiArrowBack, BiMicrophone } from 'react-icons/bi';
-import { BsGearFill } from 'react-icons/bs';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { ChevroLeft, Microphone, Settings } from '../assets/icons/icons';
 import styles from '../styles/header.module.css';
 
 export default function Header() {
   return (
     <header className={styles}>
       <div className={styles.container}>
-        <Link to="/">
-          <BiArrowBack fontSize="20px" color="#fff" />
-        </Link>
-        <h1>The Air Pollution App</h1>
+        <NavLink to="/">
+          <ChevroLeft />
+        </NavLink>
+        <h1>airpoll</h1>
         <nav>
-          <Link to="/">
-            <BiMicrophone fontSize="20px" color="#fff" />
-          </Link>
-          <NavLink to="/">
-            <BsGearFill fontSize="18px" color="#fff" />
-          </NavLink>
+          <Microphone />
+          <Settings />
         </nav>
       </div>
     </header>
