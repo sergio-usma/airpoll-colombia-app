64ea8d8ebb7b102a690a469c446d8c97b39beda4
Sergio Usma <sergioandresusma@hotmail.com>
2023-08-05 00:15:40 -0500

    Update styles for Locations component

--- src/components/Locations.jsx
+++ src/components/Locations.jsx
@@ -1,4 +1,4 @@
-import { BsFillPeopleFill } from 'react-icons/bs';
+import { BsArrowRightCircle, BsFillPeopleFill } from 'react-icons/bs';
 import PropTypes from 'prop-types';
 import styles from '../styles/locations.module.css';
 
@@ -7,6 +7,7 @@ export default function Locations({
 }) {
   return (
     <div className={styles.card} onClick={onClick} role="presentation">
+      <BsArrowRightCircle className={styles.card__icon} />
       <img className={styles.card__img} src={image} alt="This is a city" />
       <div className={styles.card__details}>
         <h2 className={styles.card__title}>{city}</h2>
